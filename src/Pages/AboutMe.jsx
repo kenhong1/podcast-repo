d2d40c8d55b6af2f238d0230e15a1ff73c65c9a5
kenneth hong <kennethhong@kenneths-MacBook-Pro.local>
2019-04-29 20:04:45 -0700

    starting working on page for about me

--- src/Pages/AboutMe.jsx
+++ src/Pages/AboutMe.jsx
@@ -1,13 +1,31 @@
 import React, {Component} from "react"
 import Footer from "../Components/Footer"
+import {Row, Col, Container} from "react-bootstrap"; 
 
 
 class AboutMe extends Component{
    render(){
       return(
          <div className="aboutMeBox">
-            <h1> About Me </h1>
-            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, expedita quia fugit, unde vero sapiente laudantium aperiam neque, nihil libero temporibus possimus iure saepe. Quos beatae sit dolorem optio ullam! </p> 
+            <h1 class> About Me </h1>
+         <Row>
+            <Col>
+               <h1> pictures and stuff</h1>
+            </Col>
+            <Col>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, expedita quia fugit, unde vero sapiente 
+               laudantium aperiam neque, nihil libero temporibus possimus iure saepe. Quos beatae sit dolorem optio ullam! 
+            </p>
+            </Col>
+         </Row> 
+            <Container className="aboutMeCont">
+                  <h1> Connect With Me! </h1>
+               <Row>
+                  <Col>
+                  <p> Fb icons and such </p>
+                  </Col>
+               </Row>
+            </Container>
             <Footer /> 
          </div>
       )
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from "react"; 
 import Footer from "../Components/Footer";
-import {Jumbotron, Button} from "react-bootstrap";
+import {Jumbotron, Button, Card, Row, Container, Col} from "react-bootstrap";
 
 
 class Home extends Component{
@@ -11,8 +11,52 @@ class Home extends Component{
                <Jumbotron className="jumbotron" fluid>
                <h1 className="homeHeader">Sam Vinson</h1>
                   <p>World Wide Niggahs</p>
+                  <p> stuff that goes here </p>
                   <a href="/about-me"><Button variant="primary">Learn more niggah </Button></a>
                </Jumbotron>
+               <Container>
+                  <Row>
+                     <Col>
+                        <Card style={{ width: '20rem' }}>   
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                              <Card.Body>
+                                 <Card.Title>Card Title</Card.Title>
+                                 <Card.Text>
+                                    Some quick example text to build on the card title and make up the bulk of
+                                    the card's content.
+                                 </Card.Text>
+                                 <Button variant="primary">Go somewhere</Button>
+                              </Card.Body>
+                        </Card>
+                     </Col>
+                     <Col>
+                        <Card style={{ width: '20rem' }}>   
+                           <Card.Img variant="top" src="holder.js/100px180" />
+                              <Card.Body>
+                              <Card.Title>Card Title</Card.Title>
+                              <Card.Text>
+                                 Some quick example text to build on the card title and make up the bulk of
+                                 the card's content.
+                              </Card.Text>
+                              <Button variant="primary">Go somewhere</Button>
+                              </Card.Body>
+                        </Card>
+                     </Col>
+                     <Col>
+                        <Card style={{ width: '20rem' }}>   
+                           <Card.Img variant="top" src="holder.js/100px180" />
+                              <Card.Body>
+                                 <Card.Title>Card Title</Card.Title>
+                                 <Card.Text>
+                                    Some quick example text to build on the card title and make up the bulk of
+                                    the card's content.
+                                 </Card.Text>
+                                 <Button variant="primary">Go somewhere</Button>
+                              </Card.Body>
+                        </Card>
+                     </Col>
+                  </Row>
+               </Container>
             </div>
          <Footer /> 
          </div>
